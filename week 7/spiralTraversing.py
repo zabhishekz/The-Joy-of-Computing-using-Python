9ebde3d850a00199d3877221fbebc0bfc73671b7
Abhishek Verma <zabhishek.verma@gmail.com>
2021-09-05 20:13:31 +0530

    week 7: spiral traversing with colored dots animation

--- week 7/spiralTraversing.py
+++ week 7/spiralTraversing.py
@@ -5,12 +5,16 @@ Created on Sun Sep  5 19:05:25 2021
 @author: Abhishek
 """
 import turtle
-
 turtle.bgcolor("black")
 seurat = turtle.Turtle()
+from random import randint
+
 width =  5
 height = 7
 dot_distance = 25
+
+seurat.penup()
+list_color=["white","yellow","brown","red","blue","green","orange","pink","violet","grey", "cyan"]
 seurat.setpos(-250,250)
 
 def spiral(m,n):
@@ -21,37 +25,52 @@ def spiral(m,n):
     '''
     k is index of starting row
     l is index of starting column
+    m is ending riw index
+    n is ending column index
+    i is iterator
     '''
+    col=randint(0,10)
+    seurat.color(list_color[col])
     while(k<m and l<n):
         if f==1:
             seurat.right(90)
             
         #print 1st row
         for i in range(l,n):
+            seurat.dot()
             seurat.forward(dot_distance)
             #print(a[k][i], end=" ")
         k+=1
         f=1
         
         seurat.right(90)
+        col=randint(0,10)
+        seurat.color(list_color[col])
         #print last column
         for i in range(k,m):
+            seurat.dot()
             seurat.forward(dot_distance)
             #print(a[i][n-1], end=" ")
         n-=1
         seurat.right(90)
         
+        col=randint(0,10)
+        seurat.color(list_color[col])
         if(k<m):
             #print last row
             for i in range(n-1, l-1, -1):
+                seurat.dot()
                 seurat.forward(dot_distance)
                 #print(a[m-1][i],end=" ")
             m -= 1
         seurat.right(90)
-            
+           
+        col=randint(0,10)
+        seurat.color(list_color[col])
         if(l<n):
             #print first column
             for i in range(m-1, k-1, -1):
+                seurat.dot()
                 seurat.forward(dot_distance)
                 #print(a[i][l], end=" ")
             l += 1
