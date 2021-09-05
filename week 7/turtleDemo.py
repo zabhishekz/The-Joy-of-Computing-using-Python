037806fcdc86082dc5d745fcfc3166aa83cb37a8
Abhishek Verma <zabhishek.verma@gmail.com>
2021-09-05 20:04:18 +0530

    Week 7: spiral traversing with animation

---
+++ week 7/turtleDemo.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  5 19:56:49 2021
+
+@author: Abhishek
+"""
+
+import turtle
+
+tur = turtle.Turtle()
+
+for i in range(50):
+    tur.forward(50)
+    tur.right(144)
+    
+turtle.done()
